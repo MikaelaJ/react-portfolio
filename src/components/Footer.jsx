3594e76e3bec0028355adf2fbe0939a227619dec
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-04-02 12:06:55 +0200

    feat: Add footer details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <nav>
+      <ul>
+        <li>
+        <Link href="#">
+                    <a target="_blank" className="btn hover:text-accent-primary-pensionera">
+                      <FontAwesomeIcon
+                        className="text-accent-secondary-pensionera mr-2 "
+                        icon={['fab', 'facebook']}
+                      />
+                      Facebook
+                    </a>
+                  </Link>
+        </li>
+        <li>LinkedIn</li>
+        <li>GitHub</li>
+      </ul>
+    </nav>
   )
 }
 
