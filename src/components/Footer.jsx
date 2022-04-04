2d8dbf9e9695b24f792626745986f3cd5880ed5e
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-04-04 21:09:16 +0200

    feat: Add LinkedIn and Instagram icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CompanyProfile from "../constants/CompanyProfile";
 
-function Footer() {
+
+export default function Footer() {
+  const profiles = [CompanyProfile.LINKEDIN, CompanyProfile.INSTAGRAM];
+  console.log(profiles)
+
   return (
     <nav>
       <ul>
-        <li>
-        <Link href="#">
-                    <a target="_blank" className="btn hover:text-accent-primary-pensionera">
-                      <FontAwesomeIcon
-                        className="text-accent-secondary-pensionera mr-2 "
-                        icon={['fab', 'facebook']}
-                      />
-                      Facebook
-                    </a>
-                  </Link>
-        </li>
+        {profiles.map((profile, i) => (
+          <li key={i}>
+            <Link href={profile.link}>
+              <a target="_blank" className="btn hover:text-accent-primary-pensionera">
+                <FontAwesomeIcon
+                  className="text-accent-secondary-pensionera mr-2 "
+                  icon={[profile.iconFamily, profile.iconName]}
+                />
+                {profile.name}
+              </a>
+            </Link>
+          </li>
+        ))}
         <li>LinkedIn</li>
         <li>GitHub</li>
       </ul>
     </nav>
   )
 }
-
-export default Footer
