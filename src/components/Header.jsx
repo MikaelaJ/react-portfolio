e41648a93d32b42870a32f717d48cc651424c8ad
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-04-08 16:39:02 +0200

    feat: Add styling tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Portfolio</li>
-          <li>About</li>
+      <nav className="flex items-center justify-between flex-wrap">
+        <ul className="py-10">
+          <li className="mr-3"><h3 className="inline">Home</h3></li>
+          <li className="mr-3"><h3 className="inline">Portfolio</h3></li>
+          <li className="mr-3"><h3 className="inline">About</h3></li>
         </ul>
       </nav>
     </header>
